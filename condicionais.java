599af16229bd8647c56c82fa82ce24c86d4b1792
122110631 FERNANDO GENTIL PINHEIRO PACHECO <fernando.gentil.pinheiro.pacheco@lcc2-55.lcc.ufcg.edu.br>
2023-04-20 14:36:57 -0300

    Mais um exercicio que fiz com o professor Douglas

---
+++ condicionais.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class condicionais {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String nome = sc.nextLine();
+
+	if (nome.equals("Douglas")) {
+		System.out.println("Eu sou o Douglas");
+	} else {
+		System.out.println("Você não é o Douglas");
+	}
+     }
+
+}
+
